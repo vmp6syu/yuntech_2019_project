5d14510f2c4d198c21b9cfc6c2af70f7b7068cac
vmp6syu <45782419+vmp6syu@users.noreply.github.com>
2019-04-17 12:47:22 +0800

    Update push.py

--- push.py
+++ push.py
@@ -1,24 +1,31 @@
 import json
+import os
+import time
 import paho.mqtt.publish as publish
 
-host="1401.125.45.146."
+
+
+host="140.125.45.146."
 topic="stauts/push"
+#client_id = ""     # If broker asks client ID.
+#user=" "
+#password" "
+#If broker asks user/password.  # If broker asks user/password.
 
-
-# If broker asks user/password.
-# auth = {'username': "", 'password': ""}
-
-# If broker asks client ID.
-#client_id = ""
+client.connect("host")
 
 filename='123.json'
 with opne(filename) as f:
   status=json.load(f)
 
-publish.single(topic, status, qos=1, hostname=host)
+  
+i=True  #   if we out to stop the connect 
+while (i):
+  client.pushlish(topic,status)
+  
 
 
-
+#publish.single(topic, status, qos=1, hostname=host)
 #publish.single(topic, payload, qos=1, host=host,
 #    auth=auth, client_id=client_id)
   
