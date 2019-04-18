ba8d6f2a47479af7e24955fda4e32e8c732e5028
vmp6syu <45782419+vmp6syu@users.noreply.github.com>
2019-04-18 20:26:20 +0800

    Update push.py

--- push.py
+++ push.py
@@ -3,29 +3,36 @@ import os
 import time
 import paho.mqtt.publish as publish
 
-
-
-host="140.125.45.146."
-topic="stauts/push"
-#client_id = ""     # If broker asks client ID.
-#user=" "
-#password" "
-#If broker asks user/password.  # If broker asks user/password.
-
-client.connect("host")
-
-filename='123.json'
-with opne(filename) as f:
-  status=json.load(f)
+def mqtt_on_push(host,topic,client_id,user,password,bn_loop):
+    client.connect(host)
+ 
+    filename='123.json'
+    with opne(filename) as f:
+        status=json.load(f)
 
   
-i=True  #   if we out to stop the connect 
-while (i):
-  client.pushlish(topic,status)
+    i=True  #   if we out to stop the connect 
+        while (i):
+            client.pushlish(topic,status)
   
 
 
 #publish.single(topic, status, qos=1, hostname=host)
 #publish.single(topic, payload, qos=1, host=host,
 #    auth=auth, client_id=client_id)
+  
+if __name__ == '__main__':
+  
+    host="140.125.45.146"
+    topic="stauts/push"
+    client_id = ""     # If broker asks client ID.
+    user=" "
+    password" "
+    If broker asks user/password.  # If broker asks user/password.
+    bn_loop=1
+    
+    mqtt_on_push(host,topic,client_id,user,password,bn_loop)
+    print "exit program"
+    sys.exit(0)
+  
   
